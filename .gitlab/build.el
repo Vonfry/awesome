3d09c946410d6b28c77efd989ef591316ad86a4b
Vonfry <mail@vonfry.name>
2021-05-26 15:37:37 +0800

    ci: set org-roam dir

--- .gitlab/build.el
+++ .gitlab/build.el
@@ -5,14 +5,19 @@
 
 (package-install 'org-roam)
 
-(setq CI_DIR (getenv "CI_PROJECT_PATH")
+(setq CI_DIR (getenv "CI_PROJECT_DIR")
       build_dir (expand-file-name ".build" CI_DIR)
       output_path (expand-file-name "roam.svg" build_dir))
 (print (concat "CI DIR: " CI_DIR))
 
+(make-directory build_dir t)
+
 (setq-default org-roam-graph-viewer
   (lambda (file)
-    (rename-file file output_path)))
+    (print (concat "graph file: " file))
+    (print (concat "new file: " output_path))
+    (copy-file file output_path t)))
+(setq-default org-roam-directory CI_DIR)
 
 (require 'org-roam)
 
