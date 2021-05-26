0d4b6b4fb2f9ea83720d4907bef208d5ee8947eb
Vonfry <mail@vonfry.name>
2021-05-26 16:02:08 +0800

    ci: add cache and test code

--- .gitlab/build.el
+++ .gitlab/build.el
@@ -1,3 +1,14 @@
+(setq ci-dir (getenv "CI_PROJECT_DIR")
+      build-dir (expand-file-name ".build" ci-dir)
+      output-file (expand-file-name "roam.svg" build-dir)
+      cache-dir (expand-file-name ".cache" ci-dir))
+(message (concat "ci dir: " ci-dir))
+(message (concat "build dir: " build-dir))
+(message (concat "cache dir: " cache-dir))
+(message (concat "output file: " output-file))
+
+(setq-default package-user-dir (expand-file-name "emacs/packages" cache-dir))
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -5,21 +16,26 @@
 
 (package-install 'org-roam)
 
-(setq CI_DIR (getenv "CI_PROJECT_DIR")
-      build_dir (expand-file-name ".build" CI_DIR)
-      output_path (expand-file-name "roam.svg" build_dir))
-(print (concat "CI DIR: " CI_DIR))
-
-(make-directory build_dir t)
-
 (setq-default org-roam-graph-viewer
   (lambda (file)
-    (print (concat "graph file: " file))
-    (print (concat "new file: " output_path))
-    (copy-file file output_path t)))
-(setq-default org-roam-directory CI_DIR
+    (message "graph file: %s" file)
+    (copy-file file output-file t)))
+(setq-default org-roam-directory ci-dir
               org-roam-graphviz-extra-options '(("rankdir" . "LR")))
 
 (require 'org-roam)
 
-(org-roam-graph)
+(setq graph-process (org-roam-graph))
+
+(while (process-live-p graph-process))
+
+(if (file-exists-p output-file)
+    (message "output: %s" output-file)
+  (progn
+    (message "no output file from org-roam-graph. ")
+    (message "process exit status: %d"
+             (process-exit-status graph-process))
+    (let ((tmp-file (car (directory-files temporary-file-directory t
+                                          "\\.svg$"))))
+      (copy-file tmp-file output-file t)
+      (message "output: %s" output-file))))
